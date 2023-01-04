4a4abee8f347a0af34808e9ce91f2d651c87e7ea
Kate1513 <kate_1513@hotmail.com>
2023-01-04 10:57:48 -0500

    Avance en mocks de HU1 y el muro de  HU2

--- src/components/wall.js
+++ src/components/wall.js
@@ -3,27 +3,38 @@ import { doc, getDoc } from 'firebase/firestore';
 import { auth, db } from '../lib/firebase';
 
 export const wall = () => {
-  let data = {};
-  onAuthStateChanged(auth, async (user) => {
-    if (user) {
-      const querySnapUser = await getDoc(doc(db, 'Users', user.uid));
-      const userObj = querySnapUser.data();
-      Object.defineProperty(userObj, 'email', { value: user.email });
-      data = userObj;
-    }
-    console.log(data.name);
-  });
+  // let data = {};
+  // onAuthStateChanged(auth, async (user) => {
+  //   if (user) {
+  // const loginUser = sessionStorage.getItem('uid');
+  // const querySnapUser = getDoc(doc(db, 'Users', loginUser));
+  // const userObj = querySnapUser.data();
+  // Object.defineProperty(userObj, 'email', { value:loginUser });
+  // data = userObj;
+  //   }
+  // });
+  // console.log(data.name);
 
   const wallSection = document.createElement('section');
   wallSection.classList.add('wall-page');
   wallSection.innerHTML = ` 
-      <section class="wall-p">
-        <figure class="dog-login">
-          <img class="dog-hi" src="images/dog-hi.png" alt="Imagen de un perrito">
-        </figure>      
-      </section> 
-      <h1>${data.name}</h1>
-      `;
+    <header class="wall-header">
+      <figure class="images-header">
+        <img class="logo-h" src="images/logo-social-pet-h.png" alt="Imagen del logo Social Pet">
+        <img class="sign-out" src="images/sign-out.svg" alt="Imagen para salir de la aplicacion">
+      </figure>      
+    </header>
+    <main>
+      <section class="wall-posting">   
+        <textarea class="input-post" placeholder="Ingrese su texto aqui..." value="" autofocus"></textarea>        
+      </section>
+      <section class="wall-posts">   
+           <h1>po</h1>     
+      </section>
+    </main>
+    <footer>
+    </footer>      
+    `;
 
   return wallSection;
 };
